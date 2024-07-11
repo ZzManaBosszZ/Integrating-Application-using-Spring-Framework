5475ab59154cc70b8bace57e581440fddaf30ffa
Phuong <phuongphuong262204@gmail.com>
2024-07-11 11:34:39 +0700

    done EmployeeManage

--- Class1/EmployeeManage/src/main/java/phuong/codeview/vn/employeemanage/controller/EmployeeController.java
+++ Class1/EmployeeManage/src/main/java/phuong/codeview/vn/employeemanage/controller/EmployeeController.java
@@ -1,17 +1,14 @@
 package phuong.codeview.vn.employeemanage.controller;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import phuong.codeview.vn.thymeleafbootstrap.model.Employee;
+import phuong.codeview.vn.employeemanage.entity.Employee;
+import phuong.codeview.vn.employeemanage.service.EmployeeService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootApplication
@@ -19,25 +16,21 @@ import java.util.List;
 @RequestMapping("/employees")
 public class EmployeeController {
 
-    private List<Employee> theEmployees;
+    private EmployeeService employeeService;
 
-    @PostConstruct
-    private void loadData() {
-
-        Employee emp1 = new Employee(1, "Phuong", "Hoang", "phuongphuong2004@gmail.com");
-        Employee emp2 = new Employee(2, "Han", "Nam", "phuongphuong2004@gmail.com");
-        Employee emp3 = new Employee(3, "Hung", "Lan", "phuongphuong2004@gmail.com");
-
-        theEmployees = new ArrayList<Employee>();
-
-        theEmployees.add(emp1);
-        theEmployees.add(emp2);
-        theEmployees.add(emp3);
-    }
+    public EmployeeController(EmployeeService theEmployeeService) { employeeService = theEmployeeService; }
 
     @GetMapping("/list")
-    public String listEmployees(Model model) {
-        model.addAttribute("employees", theEmployees);
+    public String listEmployees(Model theModel) {
+        List<Employee> theEmployees = employeeService.findAll();
+        theModel.addAttribute("employees", theEmployees);
+        return "list-employees";
+    }
+
+    @GetMapping("/add")
+    public String addEmployees(Model theModel) {
+        List<Employee> theEmployees = employeeService.findAll();
+        theModel.addAttribute("employees", theEmployees);
         return "list-employees";
     }
 }
--- Class1/EmployeeManage/src/main/java/phuong/codeview/vn/employeemanage/entity/Employee.java
+++ Class1/EmployeeManage/src/main/java/phuong/codeview/vn/employeemanage/entity/Employee.java
@@ -1,9 +1,19 @@
-package phuong.codeview.vn.employeemanage.model;
+package phuong.codeview.vn.employeemanage.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "employee")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
+    @Column(name = "first_name")
     private String firstName;
+    @Column(name = "last_name")
     private String lastName;
+    @Column(name = "email")
     private String email;
 
     public Employee(){
