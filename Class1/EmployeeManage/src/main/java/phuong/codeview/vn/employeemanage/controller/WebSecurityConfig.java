b7de1a2c8196fcc20435d4f2b77a016aa19c7f73
Phuong <phuongphuong262204@gmail.com>
2024-07-20 09:49:29 +0700

    update crud for food and login

---
+++ Class1/EmployeeManage/src/main/java/phuong/codeview/vn/employeemanage/controller/WebSecurityConfig.java
@@ -0,0 +1,46 @@
+package phuong.codeview.vn.employeemanage.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig{
+
+    @Bean
+    public DefaultSecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.authorizeHttpRequests(registry ->{
+            registry.requestMatchers("foods/list").hasRole("Admin");
+            registry.requestMatchers("login").permitAll();
+            registry.anyRequest().authenticated();
+        })
+                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails Admin = User.builder()
+                .username("phuong")
+                .password("$2a$12$fh7Vfh1mZr/hS7h9phPi4eIjQFgiHlSjwIWOQqhgljcDkVVXn8jlm")
+                .roles("Admin")
+                .build();
+        return new InMemoryUserDetailsManager(Admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
